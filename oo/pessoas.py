e43fcf4de7721109aa78c40a127f5dd39538ebb0
thiagomsales <tmsales26@gmail.com>
2020-07-22 21:04:47 -0300

    metodo vumprimentar

--- oo/pessoas.py
+++ oo/pessoas.py
@@ -1,2 +1,10 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return 'Olá'
+
+
+if __name__ == '__main__':
+     p = Pessoa()
+     print(Pessoa.cumprimentar(p))
+     print(p.cumprimentar())
+
