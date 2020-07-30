ca28843d12525f0f9fc317577af667bd9c9088bc
thiago <tmsales26@gmail.com>
2020-07-30 10:12:37 -0300

    criado carro

--- oo/carro.py
+++ oo/carro.py
@@ -1,3 +1,29 @@
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calculcar_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+    def calcular_direcao(self):
+        return self.direcao.valor
+    def girar_a_direita(self):
+        self.direcao.rotacao_a_direita_dct
+
+    def girar_a_esquerda(self):
+        self.direcao.rotacao_a_esquerda_dct
+
+
+
+
+
 class Motor:
     def __init__(self):
         self.velocidade = 0
@@ -16,10 +42,9 @@ class Direcao:
         rotacao_a_direita_dct = {
         NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
     }
-    rotacao_a_esquerda_dct = {
+        rotacao_a_esquerda_dct = {
         NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL
     }
-
     def __init__(self):
         self.valor = NORTE
 
