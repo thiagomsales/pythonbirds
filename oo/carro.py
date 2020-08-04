9cc88219c7c838d39d75cf6bd868f309fbf0d23f
thiago <tmsales26@gmail.com>
2020-08-04 11:15:34 -0300

    classe carro

--- oo/carro.py
+++ oo/carro.py
@@ -34,6 +34,30 @@ class Motor:
         self.velocidade -=2
         self.velocidade=max(0, self.velocidade)
 
+class Carro:
+    def __init__(self, direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.girar_a_esquerda()
+
+
+
 NORTE = 'Norte'
 SUL = 'Sul'
 LESTE = 'Leste'
